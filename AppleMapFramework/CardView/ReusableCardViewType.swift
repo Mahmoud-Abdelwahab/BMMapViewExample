dad19656ccd4391b0325a82a3c21ceb494f9f447
Mahmoud-Abdelwahab <mahmoudabdelwahab199390@gmail.com>
2023-07-18 13:12:09 +0300

    update using viewmodel

--- AppleMapFramework/CardView/ReusableCardViewType.swift
+++ AppleMapFramework/CardView/ReusableCardViewType.swift
@@ -9,6 +9,8 @@ import Foundation
 
 protocol ReusableCardViewType: AnyObject {
     var didTapOnCard: ((BranchCardCellViewModel) -> ())? { get set }
-    
+    var animateCamera: ((BranchCardCellViewModel) -> ())? { get set }
+
     func configureCell(viewModel: [BranchCardCellViewModel])
+    func scrollTo(desiredIndex: Int)
 }
